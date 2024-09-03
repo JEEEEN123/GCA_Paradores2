297fa5248eeea2e1b7906ee514cafec179e6c6a0
PlaylabErica <107820210+PlaylabErica@users.noreply.github.com>
2024-09-04 01:22:48 +0900

    0903

---
+++ Assets/08_Other/Boat/Scripts/BoatVRController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.XR;   
+using TMPro;
+using System;
+
+public class BoatVRController : MonoBehaviour
+{
+  public PropellerBoats ship;
+  bool forward = true;
+
+  public XRNode inputSource;
+
+  private Vector2 inputAxis;
+  private bool up = false;
+  private bool down = false;
+  private bool left = false;
+  private bool right = false;
+  //private float threshold = 0.5f;
+
+
+  void Update()
+  {
+    //get input
+    InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+    device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+
+        //find direction to move boat
+        right = inputAxis[0] > 0.5 || Input.GetKey(KeyCode.RightArrow);
+        left = inputAxis[0] < -0.5 || Input.GetKey(KeyCode.LeftArrow);
+
+        up = inputAxis[1] > 0.5 || Input.GetKey(KeyCode.UpArrow);
+        down = inputAxis[1] < -0.5 || Input.GetKey(KeyCode.DownArrow);
+    
+  
+    if (left)
+      ship.RudderLeft();
+    if (right)
+      ship.RudderRight();
+
+    if (forward)
+    {
+      if (up)
+        ship.ThrottleUp();
+      else if (down)
+      {
+        ship.ThrottleDown();
+        ship.Brake();
+      }
+    }
+    else
+    {
+      if (down)
+        ship.ThrottleUp();
+      else if (up)
+      {
+        ship.ThrottleDown();
+        ship.Brake();
+      }
+    }
+
+    if (!up && !down)
+      ship.ThrottleDown();
+
+    if (ship.engine_rpm == 0 && down && forward)
+    {
+      forward = false;
+      ship.Reverse();
+    }
+    else if (ship.engine_rpm == 0 && up && !forward)
+    {
+      forward = true;
+      ship.Reverse();
+    }
+
+
+  }
+
+}
